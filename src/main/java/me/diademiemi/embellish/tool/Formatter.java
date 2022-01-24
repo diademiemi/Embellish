13d38ba1d1970d67d4aa57d4df62e2913439fc05
diademiemi <emilia@diademiemi.me>
2022-01-24 18:38:20 +0100

    Preview presets in list

--- src/main/java/me/diademiemi/embellish/tool/Formatter.java
+++ src/main/java/me/diademiemi/embellish/tool/Formatter.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
@@ -46,8 +47,6 @@ public class Formatter {
         }
         return ChatColor.translateAlternateColorCodes('&', message);
     }
-
-    // }
 
     /**
      * Adds the copy to clipboard and run command components
@@ -90,25 +89,74 @@ public class Formatter {
         sender.spigot().sendMessage(getMessageButtons(colouredText));
     }
 
+    /**
+     * Add components for the presets list message
+     * 
+     * @param text  Text with colour codes to turn into components
+     * @param preset    Name of the preset
+     * @param label Label for the hover text
+     * @return  Return a list of base components
+     */
+    public static BaseComponent[] presetComponents(String text, String preset, String label) {
+        // Prepare component builder
+        ComponentBuilder builder = new ComponentBuilder();
+        // Create regex match for hex codes
+        final Pattern hexPattern = Pattern.compile("&#([A-Fa-f0-9]{6})");
+        Matcher matcher = hexPattern.matcher(text);
+
+        // Split text into pieces which will be separate text components (thanks, bungee)
+        String[] textList = text.split("&#([A-Fa-f0-9]{6})");
+
+        // Start at SECOND string in list, the first will always be blank
+        int counter = 1;
+
+        while (matcher.find()) {
+            // Get hex colour
+            String colour = text.substring(matcher.start(), matcher.end()).replace("&", "");
+            // Create component from text from list
+            TextComponent component = new TextComponent(textList[counter]);
+            // Set components colour
+            component.setColor(ChatColor.of(colour));
+            // Add event on hover
+            component.setHoverEvent(
+                new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(
+                        format(String.format("&rUse the &7%s &rpreset", preset)))));
+            // Add event on click
+            component.setClickEvent(
+                new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, 
+                        String.format("/%1$s preset %2$s ", label, preset)));
+            // Append component to builder
+            builder.append(component);
+            counter++;
+        }
+        // Return components
+        return builder.create();
+    }
+
+    /**
+     * Send a list of availabel presets, with hover and click actions
+     * 
+     * @param sender    Player to send the message to
+     * @param presets   List of presets
+     * @param label Label used to call the command
+     */
     public static void sendPresetsList(CommandSender sender, ArrayList<String> presets, String label) {
         ComponentBuilder builder = new ComponentBuilder();
-        
+        // If there's any presets defined
         if (presets.size() > 0 ){
             for (String p : presets) {
-                // Create clickable text for every preset
-                builder.append(
-                    new ComponentBuilder(format(String.format("&r%s", Preset.applyPreset(p, p))))
-                    .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(
-                        format(String.format("&rUse the &7%s &rpreset", p)))))
-                    .event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, 
-                        String.format("/%1$s preset %2$s ", label, p)))
-                    .create()
-                );
-                // Add the comma
-                builder.append(
-                    new ComponentBuilder(format("&7, "))
-                    .create()
-                );
+                // Only list valid presets
+                if (Preset.isValidPreset(p)) {
+                    String colouredText = Preset.applyPreset(p, p);
+                    // Create clickable text for every preset
+                    builder.append(presetComponents(colouredText, p, label));
+                    // Add the comma
+                    builder.append(
+                        new ComponentBuilder(format("&7, "))
+                        .create()
+                    );
+    
+                }
             }
         }
         sender.sendMessage(format("&7&l--- &rPresets &7&l---\n"));
